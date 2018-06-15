a35ac231d3c73e8ce05b9cd456a43c801b11daa0
Khoa T. Nguyen <khoanguyen0791@gmail.com>
2018-06-15 14:20:57 -0500

    Added some stock option symbols.

---
+++ README.md
@@ -0,0 +1,3 @@
+###This is a light java project that runs on ally-api-client. 
+
+Credits go to xgp and conniption for building the original tradeking client.
--- src/main/java/com/github/khoanguyen0791/optionAnalysis/App.java
+++ src/main/java/com/github/khoanguyen0791/optionAnalysis/App.java
@@ -1,6 +1,7 @@
 package com.github.khoanguyen0791.optionAnalysis;
 
 import java.util.ArrayList;
+import java.io.IOException;
 import java.sql.*;
 import java.util.Arrays;
 import java.util.Collection;
@@ -11,7 +12,9 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
+import org.fixprotocol.fixml.QuoteRequestMessageT;
 import org.fixprotocol.fixml.RestructuringTypeEnumT;
 
 import com.celexus.conniption.api.TradeKing;
@@ -29,7 +32,7 @@ import io.netty.handler.codec.http.HttpContentEncoder.Result;
  *
  */
 public class App {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException, IOException {
     String[] symbols = { "gnw" };
     Arrays.sort(symbols);
     String optionSymbol = "gnw";
@@ -40,11 +43,6 @@ public class App {
     // };
     String[] fids = null;
     TradeKing tk = new TradeKing(new TradeKingForeman());
-    QuotesResponse quotes = tk.quotes(symbols);
-    List<Quote> q = quotes.getQuotes().getQuote();
-    for(Quote qt: q) {
-      System.out.println(qt.getSymbol() + "\t"+qt.getAsk());
-    }
 
     // get the market clock
     // ClockResponse c = tk.clock();
@@ -53,6 +51,14 @@ public class App {
 
     // option search use case
     QuotesResponse quotesResponse = tk.options(optionSymbol, queries, fids);
+
+    // while (true) {
+    QuotesResponse quotes = tk.quotes(symbols);
+    List<Quote> q = quotes.getQuotes().getQuote();
+    for (Quote qt : q) {
+      System.out.println(qt.getSymbol() + "\t" + qt.getBid() + "\t" + qt.getAsk());
+    }
+
     List<Quote> list = quotesResponse.getQuotes().getQuote();
     list.sort(new Comparator<Quote>() {
 
@@ -74,16 +80,66 @@ public class App {
       }
 
     });
-    double endPrice = 5.43;
+    // double endPrice = 127.5;
+    double endPrice = 5.41; // 5.43 but price never gets there.
     for (Quote quote : list) {
       double breakeven = quote.getStrikeprice() + quote.getAsk();
-      double profitPercent = (endPrice - breakeven) / quote.getAsk() * 100;
-      if (profitPercent > 0.0 && quote.getXmonth() == 7.0 && quote.getXday() == 27.0) {
+      double profitMultiple = (endPrice - breakeven) / quote.getAsk() * 100;
+      if (profitMultiple > 0.0) {
 
-        System.out.println(quote.getAsk() + "\t" + quote.getStrikeprice() + "\t" + profitPercent + "\t" + quote.getBid()
-            + "\t" + quote.getAsk() + "\t" + quote.getXday() + "\t" + quote.getXmonth() + "\t" + quote.getXyear());
+        System.out.println(quote.getAsk() + "\t" + quote.getStrikeprice() + "\t" + profitMultiple + "\t" + breakeven
+            + "\t" + "\t" + quote.getXday() + "\t" + quote.getXmonth() + "\t" + quote.getXyear());
       }
     }
+
+    System.out.println("NXPI: ");
+    optionSymbol = "nxpi";
+    quotesResponse = tk.options(optionSymbol, queries, fids);
+    list = quotesResponse.getQuotes().getQuote();
+    endPrice = 127.5;
+    for (Quote quote : list) {
+      double breakeven = quote.getStrikeprice() + quote.getAsk();
+      double profitMultiple = (endPrice - breakeven) / quote.getAsk() * 100;
+      if (profitMultiple > 0.0) {
+
+        System.out.println(quote.getAsk() + "\t" + quote.getStrikeprice() + "\t" + profitMultiple + "\t" + breakeven
+            + "\t" + "\t" + quote.getXday() + "\t" + quote.getXmonth() + "\t" + quote.getXyear());
+      }
+    }
+
+    System.out.println("\n\nUVXY: ");
+    optionSymbol = "UVXY";
+    queries.remove(queries.size() - 1);
+    queries.add(new String[] { "put_call", "eq", "put" });
+    quotesResponse = tk.options(optionSymbol, queries, fids);
+    list = quotesResponse.getQuotes().getQuote();
+    endPrice = 9.86;
+    for (Quote quote : list) {
+      double breakeven = quote.getStrikeprice() - quote.getAsk();
+      double profitMultiple = Math.abs((endPrice - breakeven)) / quote.getAsk() * 100;
+      if (breakeven > 2.5) {
+
+        System.out.println(
+            quote.getAsk() + "\t" + String.format("%.2f", breakeven) +"\t"+ quote.getStrikeprice()+ "\t" + String.format("%.2f", profitMultiple)
+                + "\t" + quote.getXday() + " " + quote.getXmonth() + " " + quote.getXyear());
+      }
+    }
+    String[] sym = { "T" };
+    quotes = tk.quotes(sym);
+    q = quotes.getQuotes().getQuote();
+    for (Quote qt : q) {
+      System.out.println(qt.getSymbol() + "\t" + qt.getAsk());
+      if (qt.getAsk() < 37.411) {
+
+        System.out.println(53.75 + 1.437 * qt.getAsk());
+      } else if (qt.getAsk() > 41.349) {
+        System.out.println(53.75 + 1.3 * qt.getAsk());
+      } else
+        System.out.println(53.75 * 2);
+    }
+
+    // TimeUnit.SECONDS.sleep(15);
+    // }
 
     // get your account
     // AccountsResponse a = tk.accounts();
