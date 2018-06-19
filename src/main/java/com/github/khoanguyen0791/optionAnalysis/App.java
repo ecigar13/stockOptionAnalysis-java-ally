6373af98cacd67f7c805af2aca5740c9cad252b9
Khoa T. Nguyen <khoanguyen0791@gmail.com>
2018-06-19 18:49:28 -0500

    Added method for call and put

--- src/main/java/com/github/khoanguyen0791/optionAnalysis/App.java
+++ src/main/java/com/github/khoanguyen0791/optionAnalysis/App.java
@@ -14,6 +14,7 @@ import java.util.TreeSet;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.http.impl.client.TargetAuthenticationStrategy;
 import org.fixprotocol.fixml.QuoteRequestMessageT;
 import org.fixprotocol.fixml.RestructuringTypeEnumT;
 
@@ -24,6 +25,7 @@ import com.celexus.conniption.model.accounts.AccountsResponse;
 import com.celexus.conniption.model.clock.ClockResponse;
 import com.celexus.conniption.model.quotes.Quote;
 import com.celexus.conniption.model.quotes.QuotesResponse;
+import com.github.scribejava.apis.TheThingsNetworkV1StagingApi;
 
 import io.netty.handler.codec.http.HttpContentEncoder.Result;
 
@@ -32,78 +34,157 @@ import io.netty.handler.codec.http.HttpContentEncoder.Result;
  *
  */
 public class App {
-  public static void main(String[] args) throws InterruptedException, IOException {
-    String[] symbols = { "nxpi" };
-    Arrays.sort(symbols);
-    String optionSymbol = "nxpi";
-    List<String[]> queries = new ArrayList<String[]>();
-    queries.add(new String[] { "strikeprice", "<", "130" });
-    queries.add(new String[] { "put_call", "eq", "call" });
-    // String[] fids = { "symbol", "ask", "bid", "xdate", "xday", "xmonth", "xyear"
-    // };
-    String[] fids = null;
-    TradeKing tk = new TradeKing(new TradeKingForeman());
+    public static void main(String[] args) throws InterruptedException, IOException {
+        String[] quoteSymbols = { "nxpi", "uvxy", "svxy", "vix", "VIXY", "TQQQ", "UDOW", "SQQQ", "SDOW", "UPRO", "SPXU",
+                "SSO", "SDS", "URTY", "SRTY", "@CL.1" };
+        String optionSymbol = "sqqq";
+        List<String[]> queries = new ArrayList<String[]>();
+        queries.add(new String[] { "strikeprice", "<", "130" });
+        queries.add(new String[] { "put_call", "eq", "put" });
 
-    // get the market clock
-    // ClockResponse c = tk.clock();
+        Comparator<Quote> comparator = new resultComparator();
+        getQuote(quoteSymbols, null, comparator);
+        getPutOption(optionSymbol, queries, null, 5, comparator);
+        // getOption(optionSymbol, queries, null, 127.5, comparator);
 
-    // options search is more complex because of query
+        // }
 
-    // option search use case
-    QuotesResponse quotesResponse = tk.options(optionSymbol, queries, fids);
+        // get your account
+        // AccountsResponse a = tk.accounts();
+        // stream market quotes
+        //
+        // Future f = tk.streamQuotes(new StreamHandler<Quote>() {
+        // public void handle(Quote quote) {
+        // System.out.println(quote.toString());
+        // }
+        // }, "TWTR", "FB");
 
-    // while (true) {
-    QuotesResponse quotes = tk.quotes(symbols);
-    List<Quote> q = quotes.getQuotes().getQuote();
-    for (Quote qt : q) {
-      System.out.println(qt.getSymbol() + "\t" + qt.getBid() + "\t" + qt.getAsk());
     }
 
-    List<Quote> list = quotesResponse.getQuotes().getQuote();
-    list.sort(new Comparator<Quote>() {
+    /**
+     * Implement abstraction of getQuote so I can pass any comparator, symbols and
+     * fids.
+     * 
+     * @param symbols
+     *            String array of symbols
+     * @param fids
+     *            string arrays of fids to get (not used very often)
+     * @param comparator
+     *            a customized comparator.
+     */
+    public static void getQuote(String[] symbols, String[] fids, Comparator<Quote> comparator) {
+        Arrays.sort(symbols);
+        TradeKing tk = new TradeKing(new TradeKingForeman());
+        QuotesResponse quotesResponse = tk.quotes(symbols, fids);
+        List<Quote> list = quotesResponse.getQuotes().getQuote();
 
-      public int compare(Quote o1, Quote o2) {
-        int result = 0;
-        result = Double.valueOf(o1.getXyear()).compareTo(Double.valueOf(o2.getXyear()));
-        if (result != 0)
-          return result;
-        result = Double.valueOf(o1.getXmonth()).compareTo(Double.valueOf(o2.getXmonth()));
-        if (result != 0)
-          return result;
-
-        result = Double.valueOf(o1.getXday()).compareTo(Double.valueOf(o2.getXday()));
-        if (result != 0)
-          return result;
-
-        result = Double.valueOf(o1.getStrikeprice()).compareTo(Double.valueOf(o2.getStrikeprice()));
-        return result;
-      }
-
-    });
-    double endPrice = 127.5;
-    // double endPrice = 5.41; // 5.43 but price never gets there.
-    for (Quote quote : list) {
-      double profit = endPrice - quote.getStrikeprice();
-      double profitMultiple = profit / quote.getAsk();
-      if (profitMultiple > 1.0) {
-
-        System.out.println(String.format("%8.2f", profitMultiple) + "\t" + quote.getAsk() + "\t" + quote.getXday() + "\t"
-            + quote.getXmonth() + "\t" + quote.getXyear() + "\t" + quote.getStrikeprice());
-      }
+        for (Quote qt : list) {
+            System.out.println(qt.getSymbol() + "\t" + qt.getBid() + "\t" + qt.getAsk());
+        }
     }
 
-    // TimeUnit.SECONDS.sleep(15);
-    // }
+    /**
+     * Implement abstraction of getOption so I can pass any comparator, query,
+     * symbol and fidsand calculate profit.
+     * 
+     * @param symbol
+     *            one single symbol.
+     * @param queries
+     *            List of string array of queries.
+     * @param fids
+     *            fields to return
+     * @param targetPrice
+     *            price to calculate profit. Using the wrong price will give the
+     *            wrong profit multiple.
+     * @param comparator
+     *            a customized comparator to sort results.
+     */
+    public static void getCallOption(String symbol, List<String[]> queries, String[] fids, double targetPrice,
+            Comparator<Quote> comparator) {
+        TradeKing tk = new TradeKing(new TradeKingForeman());
+        QuotesResponse optionResponse = tk.options(symbol, queries, fids);
+        List<Quote> list = optionResponse.getQuotes().getQuote();
+        list.sort(comparator);
 
-    // get your account
-    // AccountsResponse a = tk.accounts();
-    // stream market quotes
-    //
-    // Future f = tk.streamQuotes(new StreamHandler<Quote>() {
-    // public void handle(Quote quote) {
-    // System.out.println(quote.toString());
-    // }
-    // }, "TWTR", "FB");
+        int curDay = 0;
+        for (Quote qt : list) {
+            if (curDay != qt.getXday())
+                System.out.println("");
+            curDay = qt.getXday();
+            
+            double profit = targetPrice - qt.getStrikeprice() - qt.getAsk();
+            double profitMultiple = profit / qt.getAsk();
+            if (profitMultiple > 0.0) {
 
-  }
+                System.out.println(String.format("%8.2f", profitMultiple) + "\t" + String.format("%8.2f", qt.getAsk())
+                        + "\t" + qt.getXday() + "\t" + qt.getXmonth() + "\t" + qt.getXyear() + "\t"
+                        + String.format("%8.2f", qt.getStrikeprice()) + "\t" + qt.getSymbol());
+            }
+        }
+    }
+
+    public static void getPutOption(String symbol, List<String[]> queries, String[] fids, double targetPrice,
+            Comparator<Quote> comparator) {
+        TradeKing tk = new TradeKing(new TradeKingForeman());
+        QuotesResponse optionResponse = tk.options(symbol, queries, fids);
+        List<Quote> list = optionResponse.getQuotes().getQuote();
+        list.sort(comparator);
+        int curDay = 0;
+        
+        for (Quote qt : list) {
+            if (curDay != qt.getXday())
+                System.out.println("");
+            curDay = qt.getXday();
+            
+            double profit = qt.getStrikeprice() - targetPrice - qt.getAsk();
+            double profitMultiple = profit / qt.getAsk();
+            if (profitMultiple > 0.0) {
+
+                System.out.println(String.format("%8.2f", profitMultiple) + "\t" + String.format("%8.2f", qt.getAsk())
+                        + "\t" + qt.getXday() + "\t" + qt.getXmonth() + "\t" + qt.getXyear() + "\t"
+                        + String.format("%8.2f", qt.getStrikeprice()) + "\t" + qt.getSymbol());
+            }
+        }
+    }
+
+    /**
+     * Comparator class to sort result of all options and quotes responses.
+     * 
+     * @author khoa
+     *
+     */
+    static class resultComparator implements Comparator<Quote> {
+        @Override
+        public int compare(Quote o1, Quote o2) {
+            int result = 0;
+            result = Integer.valueOf(o1.getXyear()).compareTo(Integer.valueOf(o2.getXyear()));
+            if (result != 0)
+                return result;
+            result = Integer.valueOf(o1.getXmonth()).compareTo(Integer.valueOf(o2.getXmonth()));
+            if (result != 0)
+                return result;
+
+            result = Integer.valueOf(o1.getXday()).compareTo(Integer.valueOf(o2.getXday()));
+            if (result != 0)
+                return result;
+
+            result = Double.valueOf(o1.getStrikeprice()).compareTo(Double.valueOf(o2.getStrikeprice()));
+            return result;
+        }
+
+    }
+
+    /**
+     * Format the array of objects so it looks better when printed.
+     * 
+     * @param objectList
+     * @return List of strings already formatted.
+     */
+    public static List<String> toStringArray(List<Object> objectList) {
+        List<String> stringList = new ArrayList<>();
+        for (Object q : objectList) {
+            stringList.add(String.format("%8.2f", q));
+        }
+        return stringList;
+    }
 }
